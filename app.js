ae97addaf6ae3045a714d4720b0af144fa765f16
azarateo <azarateo@gmail.com>
2020-07-24 17:23:37 -0500

    Section 6 Lesson 34 start

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const forwardGeocoding = '/geocoding/v5/'+endpoint+'/'+searchText+'.json'
 const access_token = '?access_token=pk.eyJ1IjoiYXphcmF0ZW8iLCJhIjoiY2o4cmd4dTFuMHZ4cTJxbXBvM29wYW0zeCJ9.KzONZERt0JHUwnZvILynjw'
 const options = '&limit=1'
 const url= baseurl2+forwardGeocoding+access_token+options
-//const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Bogota.json?access_token=pk.eyJ1IjoiYXphcmF0ZW8iLCJhIjoiY2o4cmd4dTFuMHZ4cTJxbXBvM29wYW0zeCJ9.KzONZERt0JHUwnZvILynjw'
+// const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Bogota.json?access_token=pk.eyJ1IjoiYXphcmF0ZW8iLCJhIjoiY2o4cmd4dTFuMHZ4cTJxbXBvM29wYW0zeCJ9.KzONZERt0JHUwnZvILynjw'
 
 request({url: url, json: true},(error,response) =>{
     if(!error){
@@ -16,11 +16,11 @@ request({url: url, json: true},(error,response) =>{
         const latitude = data.features[0].center[1]
         const longitude = data.features[0].center[0]
         const latlng = latitude+','+longitude
-        debugger
 
         const urlbase = 'http://api.weatherstack.com/current?'
         const access_key= 'access_key=55739ade495c57d1838c21d35fa4eb58'
-        const query = '&query='+latlng+'&units=m'
+        const query = '&query=&units=m'
+        //const query = '&query='+latlng+'&units=m'
         const url = urlbase +  access_key + query
         
         request({ url: url, json: true},(error,response)=>{
@@ -33,7 +33,11 @@ request({url: url, json: true},(error,response) =>{
             
         })
 
-    }else{
+    }
+    else if(response.body.error){
+        console.log('There was an error trying to get that location: '+response.body.error.info)
+    }
+    else{
         console.log('Something went wrong')
     }
 })
