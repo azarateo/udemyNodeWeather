966c31d2b2c7e7311288bc9f22f128d89bbe4bfb
azarateo <azarateo@gmail.com>
2020-07-24 14:01:43 -0500

    Section 6 Lesson 32 - Challenge solved

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const request = require('postman-request')
 const url = 'http://api.weatherstack.com/current?access_key=55739ade495c57d1838c21d35fa4eb58&query=Bogota'
 
-request({ url: url},(error,response)=>{
+request({ url: url, json: true},(error,response)=>{
     if(!error){
-        const data = JSON.parse(response.body)
-        console.log(data.current)
+        const data = response.body
+        console.log('In '+data.request.query+' the temperature is '+data.current.temperature+' degrees and it feels like '+data.current.feelslike)
     }else{
         console.log('Something went wrong')
     }
