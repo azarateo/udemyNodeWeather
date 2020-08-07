1caf8ccac01fd2782b19a6aaf6d176cc2723534f
azarateo <azarateo@gmail.com>
2020-08-07 18:23:35 -0500

    Done

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 const { geocode } = require('./utils/geocode.js')
 const { forecast } = require('./utils/forecast.js')
 
-var argument = 'Bogota'
+var address =argument = process.argv[2]
 
-if(process.argv.length >= 3){
-   argument = process.argv[2]
+if(!address){
+   console.log('Please provide an address')
+}else{
+    geocode(argument,(error, {latitude, longitude, location})=>{
+        if(error){
+            return console.log(error)
+        }else{
+            forecast(latitude,longitude,(error,data)=>{
+                if (error) {
+                    return console.log(error)
+                }else{
+                    console.log(location)
+                    console.log(data)
+                }
+            })  
+        }
+    })
+    
+    
 }
 
-
-geocode(argument,(error, {latitude, longitude, location})=>{
-    if(error){
-        return console.log(error)
-    }else{
-        forecast(latitude,longitude,(error,data)=>{
-            if (error) {
-                return console.log(error)
-            }else{
-                console.log(location)
-                console.log(data)
-            }
-        })  
-    }
-})
 
